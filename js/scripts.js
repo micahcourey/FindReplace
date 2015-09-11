878bfd88aa545a61db693f4aed769e3536907f65
micahcourey <micahjcourey@gmail.com>
2015-09-11 11:43:36 -0700

    add jquery script to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,20 @@ var findReplace = function(string, search_word, replacement) {
   var replacement_string = string.replace(search_word, replacement);
   return replacement_string;
 };
+
+$(document).ready(function() {
+  $("form#find_replace").submit(function(event) {
+    var string = $("input#string").val();
+    var search_word = $("input#search_word").val();
+    var replacement = $("input#replacement").val();
+    var result = findReplace(string, search_word, replacement);
+
+    $(".string").text(string);
+    $(".search_word").text(search_word);
+    $(".replacement").text(replacement);
+    $(".result").text(result);
+
+    $("#result").show();
+    event.preventDefault();
+  });
+});
