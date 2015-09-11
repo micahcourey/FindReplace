33aadb11132e5bcec95584500e29f1ed72a865b4
micahcourey <micahjcourey@gmail.com>
2015-09-11 10:03:03 -0700

    pass second test to input a string and return a string

--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,10 @@
 describe('findReplace', function() {
-  it("input a word and output that word", function() {
+  it("inputs a word and outputs that word", function() {
     expect(findReplace("Micah")).to.equal("Micah");
+  })
+
+  it("inputs a string and outputs that string", function() {
+    expect(findReplace("Hi, My name is Micah.")).to.equal("Hi, My name is Micah.");
   })
 
 })
