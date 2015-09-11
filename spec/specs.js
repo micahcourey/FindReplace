4aa989cd36bd2fa6493f7d7d4e24d506a18ac990
micahcourey <micahjcourey@gmail.com>
2015-09-11 11:10:50 -0700

    pass test to replace words even with case diffences and puncuation attached

--- spec/specs.js
+++ spec/specs.js
@@ -11,12 +11,16 @@ describe('findReplace', function() {
     expect(findReplace("Micah", "Micah", "Courey")).to.equal("Courey");
   })
 
-  it("inputs a string, a search word and a  replacement word and outputs the replacement word in the string", function() {
+  it("inputs a string, a search word and a replacement word and outputs the replacement word in the string", function() {
     expect(findReplace("Hi, My name is Micah.", "Micah", "Heisendurrrrg")).to.equal("Hi, My name is Heisendurrrrg.");
   })
 
-  it("inputs a string, a search word and a  replacement word and outputs multiple instances or the replacement word in the string", function() {
+  it("inputs a string, a search word and a replacement word and outputs the string with multiple replacement instances of the replacement word in the string", function() {
     expect(findReplace("Hi, My name is Micah. My full name is Micah Courey", "Micah", "Heisendurrrrg")).to.equal("Hi, My name is Heisendurrrrg. My full name is Heisendurrrrg Courey");
   })
 
-})
+  it("inputs a string with a search word and a replacement word and outputs multiple instances or the replacement word even when replacement word has case diffences and puncuation attached to the word", function() {
+    expect(findReplace("Hi, My name is Micah. My full name is micah Courey. You can call me mIcAh!", "Micah", "Heisendurrrrg")).to.equal("Hi, My name is Heisendurrrrg. My full name is Heisendurrrrg Courey. You can call me Heisendurrrrg!");
+  })
+
+});
